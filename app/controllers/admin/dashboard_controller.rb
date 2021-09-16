d1d6c17f55dcfce90eac58d91eea2a30369033fe
Lliam McBean <lliam.mcbean@gmail.com>
2021-09-16 03:08:42 +0000

    created product and category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @product_total = Product.order(id: :desc).count
+    @product_categories = Category.order(id: :desc).count
   end
 end
